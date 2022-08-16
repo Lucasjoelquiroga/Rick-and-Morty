7b9a50eda2021c7c8552a05b70935be8c90627fc
lucas joel quiroga <lucasjoelquiroga@gmail.com>
2022-08-16 14:50:01 -0300

    275

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const handleSubmit = e => {
     <div className="App">
       <div className='div-header'>
    <div className='div-img'>
-    <img src="/image2.png" alt="" />
+    <img className='img' src="/image2.png" alt="" />
    </div>
    <form className='form' onSubmit={handleSubmit} autoComplete='off'>
     <input className='input' id='search' type="text" placeholder='type a location id'/>
